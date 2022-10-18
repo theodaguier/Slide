81bda824a85e3d505ea1c00b1485fa5a303104b1
theodaguier <92515854+theodaguier@users.noreply.github.com>
2022-10-18 14:37:37 +0200

    Initial Commit

---
+++ Slide/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Slide
+//
+//  Created by Théo Daguier on 18/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slide/SlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideApp.swift
+//  Slide
+//
+//  Created by Théo Daguier on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
