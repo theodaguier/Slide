e89a3556e5f012bb30f921b4298b2e828a3936c6
theodaguier <92515854+theodaguier@users.noreply.github.com>
2022-10-18 16:17:11 +0200

    Initial Commit

--- Slide/ContentView.swift
+++ Slide/ContentView.swift
@@ -5,17 +5,37 @@
 //  Created by Théo Daguier on 18/10/2022.
 //
 
+////Recherche
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            NavigationView {
+                ZStack {
+                    // Background
+                    Color(#colorLiteral(red: 0.03529411926865578, green: 0.07058823853731155, blue: 0.15294118225574493, alpha: 1)).edgesIgnoringSafeArea(.all)
+                    VStack {
+                        // Header
+                        HStack {
+                            Image(systemName: "arrow.backward")
+                                .resizable()
+                                .foregroundColor(Color.white)
+                                .frame(width: 24.0, height: 24.0)
+                            Spacer()
+                            Text("Choix du genre").font(.system(size: 20, weight: .bold)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).tracking(-0.41).multilineTextAlignment(.center)
+                            Spacer()
+                        }
+                        HStack{
+                            HStack{
+                                Color.green
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 22))
+                            .frame(width: 158.0, height: 158.0)
+                        }
+                    }
+                }
+            }
     }
 }
 
---
+++ Slide/GenreData.swift
@@ -0,0 +1,21 @@
+//
+//  GenreData.swift
+//  Slide
+//
+//  Created by Théo Daguier on 18/10/2022.
+//
+
+/*
+import SwiftUI
+
+struct genre : Identifiable {
+    var id = UUID()
+    var title : String
+    var image : String
+}
+
+let genreData = [
+    genre(title: "POP", image: <#String#>), genre(title: "ROCK", image: <#String#>), genre(title: "COUNTRY", image: <#String#>), genre(title: "RAP", image: <#String#>) ,genre(title: "REGGAE", image: <#String#>), genre(title:"VARIÉTÉ FR", image: <#String#>)
+    
+]
+*/
